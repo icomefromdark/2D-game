cd77def1115824d7e0b9b90e4e103fa0b7fd50ce
icomefromdark <pypoy66@gmail.com>
2024-12-15 11:05:49 +0700

    fix plat

--- Assets/Assets/VerticalPlatform.cs
+++ Assets/Assets/VerticalPlatform.cs
@@ -6,12 +6,12 @@ public class VerticalPlatform : MonoBehaviour
 {
     private PlatformEffector2D effector;
     public float waitTime;
+    private bool isRotated = false;
 
     void Start()
     {
         effector = GetComponent<PlatformEffector2D>();
     }
-
 
     void Update()
     {
@@ -19,11 +19,13 @@ public class VerticalPlatform : MonoBehaviour
         {
             waitTime = 0.5f;
         }
+
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             if (waitTime <= 0)
             {
                 effector.rotationalOffset = 180;
+                isRotated = true;
                 waitTime = 0.5f;
             }
             else
@@ -31,9 +33,11 @@ public class VerticalPlatform : MonoBehaviour
                 waitTime -= Time.deltaTime;
             }
         }
-        if (Input.GetKey(KeyCode.Space))
+        else if (isRotated)
         {
             effector.rotationalOffset = 0;
+            isRotated = false;
         }
     }
 }
+
